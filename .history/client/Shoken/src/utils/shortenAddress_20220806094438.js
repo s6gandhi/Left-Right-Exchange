0a53446dd0124325c38f301c391e03d2f38886fa
s6gandhi <99891675+s6gandhi@users.noreply.github.com>
2022-08-09 17:27:38 -0400

    Delete .history directory

--- .history/client/Shoken/src/utils/shortenAddress_20220806094438.js
+++
@@ -1,7 +0,0 @@
-export const shortenAddress = (address) => `${address.slice(0, 5)}...${address.slice(address.length - 4)
-    
-
-    import abi from "./Transactions.json";
-
-export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
-export const contractABI = abi.abi;
